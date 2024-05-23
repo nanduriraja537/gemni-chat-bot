b12c839a678e2b8e8f37bc928319c8adb334b10a
Raja Rama Chandra Kumar <rajarama_nanduri@epam.com>
2024-05-23 13:49:43 +0530

    updated

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 # Use the PORT environment variable, default to 8000 if not set
 PORT=${PORT:-8000}
 
-streamlit run app.py --server.port $PORT --server.enableCORS false
+streamlit run gemni-read-dataset.py --server.port $PORT --server.enableCORS false
