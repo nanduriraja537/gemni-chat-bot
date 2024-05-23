01fd522d3abcdf0b468365a4a92ccf4e738e763b
Raja Rama Chandra Kumar <rajarama_nanduri@epam.com>
2024-05-23 14:29:31 +0530

    updated instructions

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
+pip install google-generative-ai
 # Use the PORT environment variable, default to 8000 if not set
 PORT=${PORT:-8000}
 
